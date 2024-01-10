1c1a2d9d5e97923b86f6f2618ec5692e484e261b
Alexander Drottsgård <alexander.drottsgard@eletive.com>
2024-01-10 18:48:42 +0100

    configure onedark
    for some reason setting light as initial style does not work

--- init.lua
+++ init.lua
@@ -157,6 +157,14 @@ require('lazy').setup({
     'navarasu/onedark.nvim',
     priority = 1000,
     config = function()
+      require('onedark').setup({
+        style = 'warmer',
+        toggle_style_key = '<leader>ts',                                               -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+        toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' }, -- List of styles to toggle between
+        lualine = {
+          transparent = false,                                                         -- lualine center bar transparency
+        },
+      })
       vim.cmd.colorscheme 'onedark'
     end,
 
